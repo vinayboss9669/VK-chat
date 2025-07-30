a45a1527c593e1aa92bdba2ddc8a916b80b1ab1c
Vinaykumar96 <vinayboss9669@gmail.com>
2025-07-30 12:40:05 +0530

    some minor bug fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,8 @@ import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store);
 
-export const BASE_URL="http://localhost:8080"
+export const BASE_URL=`${process.env.REACT_APP_BACKEND_HOST}`
+console.log(BASE_URL)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
